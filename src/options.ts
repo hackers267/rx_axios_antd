8988a3ca3b3e66f6d0d24ef9f0ebd1f5768cb0f0
silence_zhpf <silence_zhpf@aliyun.com>
2023-03-07 14:10:38 +0800

    config: 🔥 去除无用的代码

--- src/options.ts
+++ src/options.ts
@@ -1,10 +1,33 @@
-import { type Observable } from "rxjs";
-import { type Option, pickData, toOption } from "antd-observable";
+import {type Observable} from "rxjs";
+import {type Option, pickData, toOption} from "antd-observable";
 import rxAxios from "@silence_zhpf/rx_axios";
 
+/**
+ * 直接查询options,一般用于直接返回的data字段是一个字符串或数字数组
+ * @public
+ * @param url - 请求地址
+ */
 export function options(url: string): Observable<Option[]>;
+/**
+ * 把对象数组中的某个字段值同时映射为options中的value和label选项
+ * @public
+ * @param url - 请求地址
+ * @param field - 同时作为value和label的字段名
+ */
 export function options(url: string, field: string): Observable<Option[]>;
+/**
+ * 把对象数组中的指定的两个字段值映射为options中的value和label选项
+ * @public
+ * @param url - 请求地址
+ * @param field - 需要映射的value和label
+ */
 export function options(url: string, field: Option): Observable<Option[]>;
+/**
+ * 请求options
+ * @public
+ * @param url
+ * @param config
+ */
 export function options(
   url: string,
   config?: string | Option
