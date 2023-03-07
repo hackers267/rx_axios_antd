218c7f4f2d76cef15101c5739cd851d9a48d19e8
silence_zhpf <silence_zhpf@aliyun.com>
2023-03-07 10:50:33 +0800

    chore: 🎨️ 更新jest.config.js的格式

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,26 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform:{
-    "^.+\\.tsx?$":[
-        "ts-jest",
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
       {
-        tsconfig:"tsconfig.test.json"
-      }
-    ]
+        tsconfig: "tsconfig.test.json",
+      },
+    ],
   },
-  moduleNameMapper:{
-    "^@/(.*)":"<rootDir>/src/$1"
+  moduleNameMapper: {
+    "^@/(.*)": "<rootDir>/src/$1",
   },
   collectCoverage: true,
-  coverageThreshold:{
-    global:{
+  coverageThreshold: {
+    global: {
       functions: 95,
       branches: 95,
       statements: 95,
-      lines: 95
-    }
-  }
+      lines: 95,
+    },
+  },
 };
